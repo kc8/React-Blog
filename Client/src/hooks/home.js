203700e9a873ab93be3f1e3ebbea1659911cbcfd
Kyle Cooper <kc8@users.noreply.github.com>
2020-04-05 19:16:07 -0400

    Contined development: server updates with DB connection and API implementation

--- Client/src/hooks/home.js
+++ Client/src/hooks/home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
-const Home = props => (
+const Home = props => {
+    useEffect(() => {
+      axios.get('/api/hello')
+        .then(res => setState(res.data))
+    }, [])
+
+    const [state, setState] = useState('')
+    console.log('state: ' + state)
+  return(
     <div>
-        Home
+      Home
+      <p>{state}</p>
     </div>
-)
+ )
+};
 
 export default Home;
