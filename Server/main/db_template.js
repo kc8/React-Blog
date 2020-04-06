5d5825b9b9585248fb14c54473b0f620aa17ab79
Kyle Cooper <kc8@users.noreply.github.com>
2020-04-05 20:40:22 -0400

    Continued development of server/api functions

---
+++ Server/main/db_template.js
@@ -0,0 +1,13 @@
+const {Pool} = require('pg')
+
+
+//fill in the below information with you DB creds, host, etx
+const pool = new Pool({
+    user: '', 
+    host: '',
+    database: '',
+    password: '',
+    port: 0000,
+})
+
+modeul.exports = pool
